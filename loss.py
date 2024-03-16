d8c03c76524aa69c3f0217c01b4d75d83fc03148
CodeKnight314 <rtang24@nmhschool.org>
2024-03-16 13:38:33 -0700

    Added Skeleton for loss functions

--- loss.py
+++ loss.py
@@ -4,15 +4,19 @@ import torch.nn as nn
 class FasterRCNNLoss(nn.Module): 
     def __init__(self): 
         pass 
+
     def __compute_Loss__(self): 
         pass 
+
     def forward(self, x): 
         pass
 
 class RPNLoss(nn.Module): 
     def __init__(self):
         pass
+
     def __compute_Loss__(self): 
         pass
+    
     def forward(self, x): 
         pass
