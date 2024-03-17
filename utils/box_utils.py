088bc6c2d3c1cf585523b0ff4b1f00691d4b09ae
CodeKnight314 <rtang24@nmhschool.org>
2024-03-17 02:45:22 -0700

    Added scale bounding box

--- utils/box_utils.py
+++ utils/box_utils.py
@@ -28,28 +28,53 @@ def calculate_iou(proposals: torch.Tensor, references: torch.Tensor) -> torch.Te
     return iou
 
 def calculate_iou_batch(proposals: torch.Tensor, references: List[torch.Tensor]): 
-  """
-  Calculate IOU for a batch of proposals with a batch of references.
+    """
+    Calculate IOU for a batch of proposals with a batch of references.
 
-  Args: 
-      proposals (torch.Tensor): The proposal anchors with shape (batch, number of proposals, 4)
-      references (List[torch.Tensor]): A list of reference anchors with length batch, each with shape (number of references, 4)
-  
-  Returns: 
-      List[torch.Tensor]: A list of IOU matrices. Each element contains the IOU values with shape (number of proposals, number of references)
-  """
-  batch = [] 
-  
-  # Split into separate batches
-  for i in range(proposals.size(0)): 
+    Args: 
+        proposals (torch.Tensor): The proposal anchors with shape (batch, number of proposals, 4)
+        references (List[torch.Tensor]): A list of reference anchors with length batch, each with shape (number of references, 4)
+    
+    Returns: 
+        List[torch.Tensor]: A list of IOU matrices. Each element contains the IOU values with shape (number of proposals, number of references)
+    """
+    batch = [] 
+    
+    # Split into separate batches
+    for i in range(proposals.size(0)): 
 
-    # Reference specific batch for both proposals and references
-    prop, ref = proposals[i], references[i]
+        # Reference specific batch for both proposals and references
+        prop, ref = proposals[i], references[i]
 
-    # Calculate and append the IOU matrix at the appropiate batch_index position
-    batch.append(calculate_iou(prop,ref))
+        # Calculate and append the IOU matrix at the appropiate batch_index position
+        batch.append(calculate_iou(prop,ref))
 
-  return batch
+    return batch
+
+def scale_bounding_box(coordinates : Tuple[int], scale_factor : float): 
+    """
+    Scales boundary box to a certain scale factor 
+
+    Args:
+        coordinates (Tuple[int]): A set of coordinate values in TLBR format (xmin, ymin, xmax, ymax)
+        scale_factor (float): Scales the width and height of box while keeping Top Left corner position
+    
+    Returns: 
+        Tuple[int]: A set of scaled coordinates (xmin, ymin, xmax, ymax)
+    """
+    assert scale_factor > 0, f"[Erorr] Scale Factor should be postive but is defined as {scale_factor}"
+    
+    # Unpackage coordinates
+    xmin, ymin, xmax, ymax = coordinates
+
+    # Scale respective width and height by scale factor
+    width = (xmax - xmin) * scale_factor
+    height = (ymax - ymin) * scale_factor
+    
+    # reformat the values to TLBR format
+    xmax, ymax = xmin + width, ymax + height
+
+    return (xmin, ymin, xmax, ymax)
 
 def bbox_encode(anchors: torch.Tensor, anchor_offsets: torch.Tensor) -> torch.Tensor:
     """
