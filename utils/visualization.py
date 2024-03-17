2bc7f202031b03dc037136f4bda846e0ade4d3cc
CodeKnight314 <rtang24@nmhschool.org>
2024-03-17 11:15:05 -0400

    Added crop function

--- utils/visualization.py
+++ utils/visualization.py
@@ -28,3 +28,26 @@ def draw_boundary_box(image_path : str, coordinates : Tuple[Tuple[int]], color :
 
     if output_directory:
         cv2.imwrite(output_directory,image)    
+
+def crop_region_of_interest(image_path : str, coordinates : Tuple[int], output_directory : Union[str, None], show : bool = False):
+
+    """
+    Crops image based on region of interest coordinates
+
+    Args: 
+        image_path (str): direct directory to the image 
+        coordinates (Tuple[int]): a set of coordinates, (xmin, ymin, xmax, ymax), representing a single region of interest
+        output_directory (Union[str, None]): If directory is specified, image will be saved to specified directory
+        show (bool): Shows image and destroys window after pressing key
+    """
+    image = cv2.imread(image_path)
+    xmin, ymin, xmax, ymax = coordinates
+    cropped_image = image[ymin:ymax, xmin:xmax]
+    
+    if show: 
+            cv2.imshow(cropped_image)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+
+    if output_directory:
+        cv2.imwrite(output_directory,cropped_image)    
