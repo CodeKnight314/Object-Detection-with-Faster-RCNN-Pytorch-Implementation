0339473bf14c8521dd4cb7d8caf95c05371e0ca1
CodeKnight314 <rtang24@nmhschool.org>
2024-03-20 22:47:39 -0400

    Added documentation for write function

--- utils/log_writer.py
+++ utils/log_writer.py
@@ -10,6 +10,13 @@ class Log_writer():
         self.output_file_dir = os.path.join(self.output_dir, f"Log_{len(glob(output_directory + "/*.txt"))}_{datetime.now().strftime("%Y%m/%d_%H:%M:%S")}.txt")
 
     def write(self, epoch, **kwargs):
+        """
+        Documents losses and other values for every epoch of training 
+
+        Args: 
+            epoch (int): the epoch that corresponds to the values for documentation
+            **kwargs : loss variable name followed by value. Floats should be limited to 3 decimals
+        """
         if os.path.exists(self.output_file_dir):
             with open(self.output_file_dir, 'a') as writer: 
                 log = f"[{epoch}/{self.total_epochs}] "
