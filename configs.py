8b062bd3afc332fecf574c5bb70bb457dc15be1f
CodeKnight314 <rtang24@nmhschool.org>
2024-03-23 13:33:48 -0400

    Modified relevant files

--- configs.py
+++ configs.py
@@ -1,9 +1,28 @@
 model_path = None
 
-batch_number = 64 
+batch_number = 64
 
 lr = 1e-4 
+betas = ((0.9, 0.999))
+weight_decay = 1e-3
 
 epochs = 25 
 
 model_save_path = ""
+
+num_of_classes = None
+
+root_dir = ""
+
+image_height = 640 
+image_width = 640 
+
+annotation_dir = ""
+
+transforms = None
+
+mode = "train"
+
+train_mode = True
+
+output_dir = ""
