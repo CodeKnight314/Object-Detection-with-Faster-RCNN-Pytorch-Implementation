cf0e1082de42108f8fec00058ae66c8985a2f270
CodeKnight314 <rtang24@nmhschool.org>
2024-03-24 22:51:37 -0400

    Modified relevant files

--- configs.py
+++ configs.py
@@ -1,6 +1,6 @@
 model_path = None
 
-batch_number = 64
+batch_number = 32
 
 lr = 1e-4 
 betas = ((0.9, 0.999))
@@ -8,16 +8,16 @@ weight_decay = 1e-3
 
 epochs = 25 
 
-model_save_path = ""
+model_save_path = "/workspace/Model_save_dir/"
 
 num_of_classes = None
 
-root_dir = ""
+root_dir = "/workspace/train"
 
 image_height = 640 
 image_width = 640 
 
-annotation_dir = ""
+annotation_dir = "/workspace/train/_annotations.coco.json"
 
 transforms = None
 
@@ -25,4 +25,4 @@ mode = "train"
 
 train_mode = True
 
-output_dir = ""
+output_dir = "/workspace/Log_output_dir/"
