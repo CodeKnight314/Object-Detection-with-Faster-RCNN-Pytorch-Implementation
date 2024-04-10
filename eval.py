8077d1508623f182a7c3c80ccfc4289e1ba9617e
CodeKnight314 <rtang24@nmhschool.org>
2024-04-10 10:33:08 -0400

    Modified relevant files

--- eval.py
+++ eval.py
@@ -5,8 +5,21 @@ from utils.box_utils import calculate_iou_batch
 
 def calculate_precision(proposals : torch.Tensor, 
                         references : List[torch.Tensor],
-                        positive_iou_threshold : int = 0.7, 
-                        negative_iou_threshold : int = 0.3,): 
+                        positive_iou_threshold : float = 0.7, 
+                        negative_iou_threshold : float = 0.3,): 
+    
+    """
+    Calculating precision for a given set of proposals and references
+
+    Args: 
+        proposals (torch.Tensor): A batch of proposals with shape (batch, number of proposals, 4)
+        references (List[torch.Tensor]): A list of of reference boundary boxes (number of references, 4), with length batch
+        positive_iou_threshold (float): A float defining the positive IOU threshold for proposals and references
+        negative_iou_threshold (float): A float defining the negative IOU threshold for proposals and references
+
+    Returns: 
+        float: returns a float based on precision formula (TP/(TP+NP)) or 0.0 if TP+NP = 0
+    """
     
     iou_matrix = calculate_iou_batch(proposals=proposals, references=references)
 
