2289e959c08d83321704284e17a67c5d271e7ef0
CodeKnight314 <rtang24@nmhschool.org>
2024-04-10 10:44:36 -0400

    Modified relevant files

--- eval.py
+++ eval.py
@@ -3,8 +3,7 @@ import torch.nn as nn
 from typing import List
 from utils.box_utils import calculate_iou_batch
 
-def calculate_precision(proposals : torch.Tensor, 
-                        references : List[torch.Tensor],
+def calculate_precision(iou_matrix : torch.Tensor, 
                         positive_iou_threshold : float = 0.7, 
                         negative_iou_threshold : float = 0.3,): 
     
@@ -12,8 +11,7 @@ def calculate_precision(proposals : torch.Tensor,
     Calculating precision for a given set of proposals and references
 
     Args: 
-        proposals (torch.Tensor): A batch of proposals with shape (batch, number of proposals, 4)
-        references (List[torch.Tensor]): A list of of reference boundary boxes (number of references, 4), with length batch
+        iou_matrix (torch.Tensor): An IOU matrix of shape (batch, number of propsoals, number of references)
         positive_iou_threshold (float): A float defining the positive IOU threshold for proposals and references
         negative_iou_threshold (float): A float defining the negative IOU threshold for proposals and references
 
@@ -21,8 +19,6 @@ def calculate_precision(proposals : torch.Tensor,
         float: returns a float based on precision formula (TP/(TP+NP)) or 0.0 if TP+NP = 0
     """
     
-    iou_matrix = calculate_iou_batch(proposals=proposals, references=references)
-
     max_iou, _ = iou_matrix.max(dim = 2)
 
     true_positive = torch.sum(max_iou >= positive_iou_threshold)
@@ -31,3 +27,17 @@ def calculate_precision(proposals : torch.Tensor,
     precision = true_positive.float() / (true_positive + false_positive).float() if true_positive + false_positive > 0 else 0.0
 
     return precision
+
+def calculate_recall(iou_matrix : torch.Tensor, 
+                     positive_iou_threshold : float = 0.5): 
+    """
+    """
+
+    max_iou, _ = iou_matrix.max(dim = 2)
+
+    true_positive = torch.sum(max_iou >= positive_iou_threshold)
+    false_negative = torch.sum(max_iou <= positive_iou_threshold)
+
+    recall = true_positive.float() / (true_positive + false_negative).float() if true_positive + false_negative > 0 else 0.0 
+
+    return recall
